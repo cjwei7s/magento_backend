b98f8118699f660e2c0f1f9e3614a60855f21ead
cjwei7s <38220499+cjwei7s@users.noreply.github.com>
2018-04-17 17:30:27 +0800

    Tutorial

---
+++ app/code/Amasty/HelloWorld/Model/ResourceModel/HelloWorld/Collection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Amasty\HelloWorld\Model\ResourceModel\HelloWorld;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * Define resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Amasty\HelloWorld\Model\HelloWorld', 'Amasty\HelloWorld\Model\ResourceModel\HelloWorld');
+    }
+}
